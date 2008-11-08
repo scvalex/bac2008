ca8466fbc8feb2f72454249670ffdce0a456e495
Alexandru Scvortov <scvalex@gmail.com>
2008-11-08 12:51:19 +0200

    solutiile pentru varianta 004

---
+++ 004/III_3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nrdig(int n) {
+	int i;
+
+	for (i = 0; n; ++i)
+		n /= 10;
+	
+	return i;
+}
+
+int cmp(const void *a, const void *b) {
+	return *(int*)b - *(int*)a;
+}
+
+int main(int argc, char *argv[]) {
+	int aux, v[100], n, i;
+
+	FILE *fi = fopen("NR.TXT", "r");
+	n = 0;
+	while (fscanf(fi, "%d", &aux) != EOF)
+		if (nrdig(aux) <= 2)
+			v[n++] = aux;
+	fclose(fi);
+
+	if (n > 0) {
+		qsort(v, n, sizeof(v[0]), cmp);
+
+		for (i = 0; i < n; ++i)
+			printf("%d ", v[i]);
+		printf("\n");
+	} else {
+		printf("NU EXISTA\n");
+	}
+
+	return 0;
+}
+
---
+++ 004/III_4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int cif(int n, int b) {
+	int cnt = 0;
+
+	while (n) {
+		if (n % 10 == b)
+			++cnt;
+		n /= 10;
+	}
+
+	return cnt;
+}
+
+int main(int argc, char *argv[]) {
+	int n, nr, cnti, i;
+
+	scanf("%d", &n);
+
+	cnti = 0;
+	for (i = 9; i >= 0; --i) {
+		nr = cif(n, i);
+		if (nr % 2 == 1)
+			++cnti;
+	}
+
+	if (cnti > 1) {
+		printf("0\n");
+	} else {
+		for (i = 1; i <= 9; ++i) {
+			nr = cif(n, i);
+			while (nr) {
+				printf("%d", i);
+				nr -= 2;
+			}
+		}
+		for (i = 9; i >= 1; --i) {
+			nr = cif(n, i);
+			while (nr) {
+				printf("%d", i);
+				nr -= 2;
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
